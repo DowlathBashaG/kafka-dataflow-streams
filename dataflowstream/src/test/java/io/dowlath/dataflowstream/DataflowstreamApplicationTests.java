57161cae5c07d08af99aacec60417d4d2b84e877
DowlathBashaG <9671419+DowlathBashaG@users.noreply.github.com>
2024-03-25 16:50:55 +0530

    Add files via upload
    initial commit

---
+++ dataflowstream/src/test/java/io/dowlath/dataflowstream/DataflowstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dowlath.dataflowstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataflowstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
